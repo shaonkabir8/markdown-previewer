33fe9d74923cac713bbcba1ba99ad69b49435fed
Shaonkabir <shaonkabir98@gmail.com>
2020-02-10 17:12:05 +0600

    Fixed: delete dummy content

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Markdown Preview Application</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur soluta quia autem numquam iusto eligendi quasi. Asperiores corporis cupiditate qui vero. Maiores rerum officiis, doloribus. Fugiat hic eos tempore error, ullam dolore. Odio nihil est ad sunt error, cupiditate consequuntur nesciunt voluptas voluptatibus, soluta beatae quibusdam culpa architecto consectetur eaque.</p>
-
-      <button>Click here</button>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className="markdown-preview">
+			<div class="container">
+				<h3 class="text-center my-3">Markdown Previewer</h3>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="editor">
+							<h3>Markdown Code</h3>
+							<textarea name="code" id="markdownCode"></textarea>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="output">
+							<h3>Output</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
-
 export default App;
