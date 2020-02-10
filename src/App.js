9cd5376c39aa46e05b86197813a83f0b3944c6fc
Shaonkabir <shaonkabir98@gmail.com>
2020-02-10 19:49:13 +0600

    Refactor: add initial markup for markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React,{ useState } from 'react';
+const marked = require("marked");
+
+
 
 const App = () => {
+
+
+
+	const placholderMessage = `
+	# This is React Markdown Previewer Project
+	## This is a sub-heading...
+	### And here's some other cool stuff:
+	Heres some code
+	`;
+
+	const [ text, setText ] = useState(placholderMessage)
+
+
+	const handleChange = (e) => {
+		setText(e.target.value)
+	}
 	return (
 		<div className="markdown-preview">
-			<div class="container">
-				<h3 class="text-center my-3">Markdown Previewer</h3>
-				<div class="row">
-					<div class="col-md-6">
-						<div class="editor">
+			<div className="container">
+				<h3 className="text-center my-3">Markdown Previewer</h3>
+				<div className="row">
+					<div className="col-md-6">
+						<div className="editor">
 							<h3>Markdown Code</h3>
-							<textarea name="code" id="markdownCode"></textarea>
+							<textarea name="code" id="markdownCode" onChange={handleChange}>{text}</textarea>
 						</div>
 					</div>
-					<div class="col-md-6">
-						<div class="output">
+					<div className="col-md-6">
+						<div className="output-section">
 							<h3>Output</h3>
+				            <div className="output" dangerouslySetInnerHTML = {{__html: marked(text)}}></div>
 						</div>
 					</div>
 				</div>
